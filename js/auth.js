5fd00268c3b2dc002928b8bb7636ca040b268383
Gwyndolyn Marchant <luna@shadenexus.com>
2020-02-01 18:37:31 -0500

    what if the button did something

--- js/auth.js
+++ js/auth.js
@@ -5,51 +5,58 @@ var result = location.hash.split("&");
 	// result[2] = expires_in=###
 	// result[3] = state=###
 
-var access_token = result[0].split("=")[1];
-var token_type = result[1].split("=")[1];
-var expires_in = result[2].split("=")[1];
+	var access_token = result[0].split("=")[1];
+	var token_type = result[1].split("=")[1];
+	var expires_in = result[2].split("=")[1];
 //var state = result[3].split("=")[1];
 
 fetch("https://api.spotify.com/v1/audio-analysis/6EJiVf7U0p1BBfs0qqeb1f", {
-  method: "GET",
-  headers: {
-    Authorization: `Bearer ${access_token}`     
-  }
+	method: "GET",
+	headers: {
+		Authorization: `Bearer ${access_token}`     
+	}
 })
 .then(response => { console.debug(response.json()) });
 
+var buttonPP = document.getElementById("playpause");
+
 window.onSpotifyWebPlaybackSDKReady = () => {
-	  const player = new Spotify.Player({
-	    name: "Spotify Web Playback SDK",
-	    getOAuthToken: callback => { callback(access_token); }
-	  });
+	const player = new Spotify.Player({
+		name: "Spotify Web Playback SDK",
+		getOAuthToken: callback => { callback(access_token); }
+	});
 
-	  // Error handling
-	  player.addListener('initialization_error', ({ message }) => { console.error(message); });
-	  player.addListener('authentication_error', ({ message }) => { console.error(message); });
-	  player.addListener('account_error', ({ message }) => { console.error(message); });
-	  player.addListener('playback_error', ({ message }) => { console.error(message); });
+	// Error handling
+	player.addListener('initialization_error', ({ message }) => { console.error(message); });
+	player.addListener('authentication_error', ({ message }) => { console.error(message); });
+	player.addListener('account_error', ({ message }) => { console.error(message); });
+	player.addListener('playback_error', ({ message }) => { console.error(message); });
 
-	   // Playback status updates
-	  player.addListener('player_state_changed', state => {
-	  	console.log(state);
-	  	if (state.paused) {
-	  		document.getElementById("playpause").innerHTML = "▶️ Play"
-	  	} else {
-	  		document.getElementById("playpause").innerHTML = "⏸️ Pause"
-	  	}
-	  });
+	// Playback status updates
+	player.addListener('player_state_changed', state => {
+		console.log(state);
+		if (state.paused) {
+			buttonPP.innerHTML = "▶️ Play"
+		} else {
+			buttonPP.innerHTML = "⏸️ Pause"
+		}
+	});
 
-	  // Ready
-	  player.addListener('ready', ({ device_id }) => {
-	    console.log('Ready with Device ID', device_id);
-	  });
+	// Ready
+	player.addListener('ready', ({ device_id }) => {
+		console.log('Ready with Device ID', device_id);
+	});
 
-	  // Not Ready
-	  player.addListener('not_ready', ({ device_id }) => {
-	    console.log('Device ID has gone offline', device_id);
-	  });
+	// Not Ready
+	player.addListener('not_ready', ({ device_id }) => {
+		console.log('Device ID has gone offline', device_id);
+	});
 
-	  // Connect to the player!
-	  player.connect();
+
+	buttonPP.onclick = function() {
+		player.togglePlay().then(() => console.log('Toggled playback'));
 	};
+
+	// Connect to the player!
+	player.connect();
+};
