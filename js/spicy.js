00be8906e951aa21e714490c0055fa0f300eeb2e
Gwyndolyn Marchant <luna@shadenexus.com>
2020-02-02 10:46:04 -0500

    animating pepper

--- js/spicy.js
+++ js/spicy.js
@@ -1,16 +1,14 @@
 var renderer, scene, camera, pepper;
+var ww = window.innerWidth, wh = window.innerHeight;
 
 function init(){
-
-	var ww = window.innerWidth, wh = window.innerHeight;
-
 	renderer = new THREE.WebGLRenderer();
 	renderer.setSize(ww, wh);
 	document.body.appendChild( renderer.domElement );
 	scene = new THREE.Scene();
 
-	camera = new THREE.PerspectiveCamera(50,ww/wh, 0.1, 10000);
-	camera.position.set(50,0,20);
+	camera = new THREE.PerspectiveCamera(50, ww/wh, 0.1, 10000);
+	camera.position.set(0,0,50);
 	camera.lookAt(0,0,0)
 	scene.add(camera);
 
@@ -39,23 +37,26 @@ function loadOBJ(){
 		console.log( 'There was an error loading ' + url );
 	};
 
+	var material = new THREE.MeshLambertMaterial( { color: 0x00ff00, emissive: 0x00AA00 } );
+
 	var loader = new THREE.OBJLoader(manager);
 	// load a resource
 	loader.load(
 		'pepper/pepper.obj',
 		//called when loading is done
-		function( pepper ){
-			pepper.rotation.x = Math.PI/2;
+		function( mesh ){
+			//mesh.rotation.x = Math.PI/2;
 			//Go through all children of the loaded object and search for a Mesh
-			pepper.traverse( function ( child ) {
+			mesh.traverse( function ( child ) {
 				//This allow us to check if the children is an instance of the Mesh constructor
 				if(child instanceof THREE.Mesh){
-					child.material.color = 0X006600;
+					child.material = material;
 					//Sometimes there are some vertex normals missing in the .obj files, ThreeJs will compute them
 					child.geometry.computeVertexNormals();
 				}
 			});
 			//Add the 3D object in the scene
+			pepper = mesh;
 			scene.add(pepper);
 			renderer.render(scene, camera);
 		},
@@ -68,5 +69,49 @@ function loadOBJ(){
 			console.log( 'An error happened' );
 		}
 	)
+	animate();
 };
+
+var rotation_x = 0.01;
+var rotation_y = 0.02;
+var rotation_z = 0.03;
+
+var move_x = 0.1;
+var move_y = 0.1;
+
+var scale = 4.0;
+
+var animate = function () {
+	requestAnimationFrame( animate );
+
+	pepper.rotation.x += rotation_x;
+	pepper.rotation.y += rotation_y;
+	pepper.rotation.y += rotation_z;
+
+	if (pepper.position.x > ww/40) {
+		move_x = -Math.abs(move_x);
+		pepper.position.x = ww/40;
+	}
+	if (pepper.position.y > wh/40) {
+		move_y = -Math.abs(move_y);
+		pepper.position.y = wh/40;
+	} 
+
+	if (pepper.position.x < -ww/40) {
+		move_x = Math.abs(move_x);
+		pepper.position.x = -ww/40;	
+	} 
+	if (pepper.position.y < -wh/40) {
+		move_y = Math.abs(move_y);
+		pepper.position.y = -wh/40;
+	}
+
+	pepper.position.x += move_x;
+	pepper.position.y += move_y;
+
+	pepper.scale = scale;
+
+	renderer.render( scene, camera );
+}
+
 init();
